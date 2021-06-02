21a84eee853828f68d30623bbb142d64b38914bd
RedMaun <deb.il.dpkg@gmail.com>
2021-06-02 23:55:56 +0700

    network.sh

--- distros/arch/network.sh
+++ distros/arch/network.sh
@@ -1,5 +1,9 @@
 #/bin/bash
 
+echo "Choose connection type[wifi/wired]"
+read con
+if [[ $con == "wired" ]]
+then
 interface=$(ip link | grep BROADCAST |  grep -o -P '(?<=:).*(?=:)' | sed 's: ::g')
 cp /etc/netctl/examples/ethernet-dhcp /etc/netctl
 sed s/Interface=eth0/Interface=$interface/g /etc/netctl/examples/ethernet-dhcp > /etc/netctl/ethernet-dhcp
@@ -9,3 +13,24 @@ systemctl enable netctl-auto@ethernet-dhcp.service
 systemctl enable netctl-ifplugd@ethernet-dhcp.service
 
 reboot
+elif [[ $con == "wifi" ]]
+then
+echo 'Enter SSID(if it contains $ symbol put \ before it)' 
+read ssid
+
+echo "Enter passphrase"
+read pass
+
+systemctl enable NetworkManager.service
+systemctl start NetworkManager.service
+sleep 3
+a = $(nmcli device wifi list | wc -l)
+while [ $a < 3 ]
+do 
+a = $(nmcli device wifi list | wc -l)
+sleep 1
+done
+nmcli device wifi connect $ssid password $pass
+dhcpcd
+reboot
+fi
